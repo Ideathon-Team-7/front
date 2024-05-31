669ec7b87742eed1af71cf0fc3e812d8f66c1998
egg-silver <v6984@naver.com>
2024-06-01 01:39:37 +0900

    유튜브 test 완

--- front/src/pages/PlayPage.jsx
+++ front/src/pages/PlayPage.jsx
@@ -1,3 +1,20 @@
+import YouTube from 'react-youtube';
+
 export default function PlayPage() {
-  return (<div>PlayPage</div>);
+  return (
+    <div>
+      <YouTube
+        videoId='hOJ76cZEt08' //동영상 주소
+        opts={{
+          width: '100%',
+          height: '270px',
+          playerVars: {
+            autoplay: 1, //자동 재생 여부
+            modestbranding: 1, //컨트롤 바에 유튜브 로고 표시 여부
+            loop: 1, //반복 재생
+          },
+        }}
+      />
+    </div>
+  );
 }
